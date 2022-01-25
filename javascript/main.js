557d35f2d90397370eb1d24ff7e3238486fd0e17
baothanh210 <dlbt0210@gmail.com>
2022-01-25 11:31:24 +0700

    upload file

--- javascript/main.js
+++ javascript/main.js
@@ -11,3 +11,35 @@ btnCloseMenu.addEventListener("click",()=>{
   mobileMenu.style.left="-100%";
   mobileMenu.style.width="0";
 })
+
+function checkModeScreen(){
+  const checkbox = document.querySelector('input[type="checkbox"]');
+  const root = document.querySelector("html");
+  function checkSystem() {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+   
+    if (prefersDark) {
+      root.classList.add("DarkMode");
+    } else {
+      root.classList.remove("DarkMode")
+    }
+  }
+
+  function initDarkMode() {
+    checkSystem();
+  
+    if (root.classList.contains("DarkMode")) {
+      checkbox.setAttribute("checked", true);
+    }
+  
+    checkbox.addEventListener("change", (event) => {
+      root.classList.toggle("DarkMode");
+      const isDarkMode = root.classList.contains("DarkMode");
+      checkbox.setAttribute("checked", isDarkMode)  
+    });
+  }
+  
+  
+  initDarkMode()
+}
+checkModeScreen()
