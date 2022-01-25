22e01b91ecdcc7099ab02bdd65d88be42a8bb81a
baothanh210 <dlbt0210@gmail.com>
2022-01-25 13:44:29 +0700

    upload file

--- javascript/main.js
+++ javascript/main.js
@@ -17,17 +17,16 @@ function checkModeScreen(){
   const root = document.querySelector("html");
   function checkSystem() {
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-   
+    
     if (prefersDark) {
       root.classList.add("DarkMode");
     } else {
       root.classList.remove("DarkMode")
     }
   }
-
+  
   function initDarkMode() {
     checkSystem();
-  
     if (root.classList.contains("DarkMode")) {
       checkbox.setAttribute("checked", true);
     }
