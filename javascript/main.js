ec1bf0bf21c8f149de0ddacb992f6a5e34e105c8
baothanh210 <dlbt0210@gmail.com>
2022-01-25 14:02:15 +0700

    upload file

--- javascript/main.js
+++ javascript/main.js
@@ -12,33 +12,35 @@ btnCloseMenu.addEventListener("click",()=>{
   mobileMenu.style.width="0";
 })
 
-function checkModeScreen(){
-  const checkbox = document.querySelector('input[type="checkbox"]');
-  const root = document.querySelector("html");
-  function checkSystem() {
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    
-    if (prefersDark) {
-      root.classList.add("DarkMode");
-    } else {
-      root.classList.remove("DarkMode")
-    }
-  }
+// function checkModeScreen(){
+//   const checkbox = document.querySelector('input[type="checkbox"]');
+//   const root = document.querySelector("html");
+
+//   function checkSystem() {
+//     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   
-  function initDarkMode() {
-    checkSystem();
-    if (root.classList.contains("DarkMode")) {
-      checkbox.setAttribute("checked", true);
-    }
-  
-    checkbox.addEventListener("change", (event) => {
-      root.classList.toggle("DarkMode");
-      const isDarkMode = root.classList.contains("DarkMode");
-      checkbox.setAttribute("checked", isDarkMode)  
-    });
-  }
-  
-  
-  initDarkMode()
-}
-checkModeScreen()
+//     if (prefersDark) {
+//       root.classList.add("DarkMode");
+//     } else {
+//       root.classList.remove("DarkMode")
+//     }
+//   }
+
+//   function initDarkMode() {
+//     checkSystem();
+
+//     if (root.classList.contains("DarkMode")) {
+//       checkbox.setAttribute("checked", true);
+//     }
+
+//     checkbox.addEventListener("change", (event) => {
+//       root.classList.toggle("DarkMode");
+//       const isDarkMode = root.classList.contains("DarkMode");
+//       checkbox.setAttribute("checked", isDarkMode)  
+//     });
+//   }
+
+
+//   initDarkMode()
+// }
+// checkModeScreen()
